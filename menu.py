0b82a1ec54f3d2108c31ef9cbcb9cddce14db177
monardo <cocoroco1>
2017-04-25 19:38:10 -0300

    saludo debienvenida TP-Lights Out

--- menu.py
+++ menu.py
@@ -1,1 +1,6 @@
 # Este va a ser el menu
+
+def saludoBienvenida():
+    print("Bienvenido al juego Lights Out")
+    print("Seleccione el modo en el que quiere jugar")
+    print("Seleccione SALIR para salir del juego")
