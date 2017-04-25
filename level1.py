c2f4b65f2c4ddabfda54897c084dc9dc472c8e49
monardo <cocoroco1>
2017-04-25 20:26:04 -0300

    Nivel 1 del juego

---
+++ level1.py
@@ -0,0 +1,9 @@
+# se desarrollara el nnivel 1 del juego
+
+def nivel1():
+    nivel1_1=[o, o, ., o, o]
+    nivel1_2=[o, ., o, ., o]
+    nivel1_3=[., o, o, o, .]
+    nivel1_4=[o, ., o, ., o]
+    nivel1_5=[o, o, ., o, o]
+
