aba659b23fa9b87018548810e38e1823d9a9c69c
monardo <aledrienisienia@gmail.com>
2017-04-27 19:49:44 -0300

    Nivel

---
+++ nivel.py
@@ -0,0 +1,14 @@
+# se desarrollara el nivel 1 del juego
+
+def getNivelUno():
+    level1_1=["o", "o", ".", "o", "o"]
+    level1_2=["o", ".", "o", ".", "o"]
+    level1_3=[".", "o", "o", "o", "."]
+    level1_4=["o", ".", "o", ".", "o"]
+    level1_5=["o", "o", ".", "o", "o"]
+
+    return [level1_1, level1_2, level1_3, level1_4, level1_5]
+
+def mostrarNivel(nivel):
+    print(nivel)
+
