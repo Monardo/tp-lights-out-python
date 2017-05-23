d36fccad26bc627ffd5bb04d9b4f7e3818766f01
Mono <aledrienisienia@gmail.com>
2017-05-23 18:19:48 -0300

    menu

--- menu.py
+++ menu.py
@@ -2,22 +2,22 @@
 import modoPredeterminado
 
 def saludoBienvenida():
-    print()
-    print()
+    print("")
+    print("")
     print("                          *************************************************************************")
     print("                          ***************  BIENVENIDO AL JUEGO LIGHTS OUT !!!!!!!  ****************")
     print("                          *************************************************************************")
-    print()
-    print()
+    print("")
+    print("")
 
-    modo=input(print("Ingrese A para jugar en modo Aleatoreo, ingrese P para jugar en modo Predeterminado o S para salir del juego"))
+    modo = raw_input("Ingrese A para jugar en modo Aleatoreo, ingrese P para jugar en modo Predeterminado o S para salir del juego  ")
     if modo == 'S' or modo == 's':
-        print("        Ud está saliendo del juego !!!!!!!!")
+        print("        Ud esta saliendo del juego !!!!!!!!")
         exit
     elif modo == 'A' or modo == 'a':
-        print("Ud. seleccionó el modo Aleatoreo")
+        print("Ud. selecciono el modo Aleatoreo")
     elif modo == 'P' or modo == 'p':
-        print("Ud seleccionó el modo Predeterminado")
+        print("Ud selecciono el modo Predeterminado")
         modoPredeterminado.iniciarModoPredeterminado()
     else:
         print("Ud ingreso un valor incorrecto, por favor ingrese nuevamente su eleccion")
