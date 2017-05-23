cc37ccc88acdc35942e945cdb45bcb9a7fb48128
Mono <aledrienisienia@gmail.com>
2017-05-23 18:20:37 -0300

    nivel

--- nivel.py
+++ nivel.py
@@ -1,20 +1,47 @@
 
-def getNivelUno():
-    level1=[
-        ["    ", "A", "B", "C", "D", "E"],
-        ["1 | ", "o", "o", ".", "o", "o"],
-        ["2 | ", "o", ".", "o", ".", "o"],
-        ["3 | ", ".", "o", "o", "o", "."],
-        ["4 | ", "o", ".", "o", ".", "o"],
-        ["5 | ", "o", "o", ".", "o", "o"],
-        ]
-    return level1
+nivel1 = [
+    ["o", "o", ".", "o", "o"],
+    ["o", ".", "o", ".", "o"],
+    [".", "o", "o", "o", "."],
+    ["o", ".", "o", ".", "o"],
+    ["o", "o", ".", "o", "o"],
+]
 
-def mostrarNivel(nivel):
-    for elemento in nivel:
-        linea = ''
-        for subelemento in elemento:
-            linea = linea +' '+ subelemento
-        print(linea)
+nivel2 = [
+    ["o", "o", "B", "o", "o"],
+    ["o", ".", "o", ".", "o"],
+    [".", "o", "o", "o", "."],
+    ["o", ".", "o", ".", "o"],
+    ["o", "o", ".", "o", "o"],
+]
 
+nivel3 = [
+    ["o", "o", ".", "o", "o"],
+    ["o", ".", "o", ".", "o"],
+    [".", "o", "o", "o", "."],
+    ["o", ".", "o", ".", "o"],
+    ["o", "o", ".", "o", "o"],
+]
+
+nivel4 = [
+    ["o", "o", ".", "o", "o"],
+    ["o", ".", "o", ".", "o"],
+    [".", "o", "o", "o", "."],
+    ["o", ".", "o", ".", "o"],
+    ["o", "o", ".", "o", "o"],
+]
+
+nivel5 = [
+    ["o", "o", ".", "o", "o"],
+    ["o", ".", "o", ".", "o"],
+    [".", "o", "o", "o", "."],
+    ["o", ".", "o", ".", "o"],
+    ["o", "o", ".", "o", "o"],
+]
+
+nivles = [nivel1,nivel2,nivel3,nivel4,nivel5]
+
+
+def getNivel(nivelactual):
+    return nivles[nivelactual-1]
 
