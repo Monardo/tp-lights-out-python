da71139e22f303fb0c1dccb4340c4e18bfa757c5
Mono <aledrienisienia@gmail.com>
2017-05-23 18:20:09 -0300

    modificomatriz

---
+++ modificoMatriz.py
@@ -0,0 +1,13 @@
+import nivel
+import modoPredeterminado
+def coordenadaCambiaMatriz():
+    nivel.getNivelUno()
+
+    x = coordenadaNumerica
+    y = coordenadaLetraTransformada
+
+    a = nivel.getNivel(nivelactual)
+    if a[x, y] == 'o':
+        level1.pop(x, y)
+
+coordenadaCambiaMatriz()
