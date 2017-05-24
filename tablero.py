d9ba5902da03532fc0a13565e16fbfb1fb4cfa36
Mono <aledrienisienia@gmail.com>
2017-05-23 21:55:09 -0300

    tablero

---
+++ tablero.py
@@ -0,0 +1,15 @@
+
+def mostrarNivel(nivel):
+    print "    ", "A", "B", "C", "D", "E"
+    ["1 | "]
+    ["2 | "]
+    ["3 | "]
+    ["4 | "]
+    ["5 | "]
+
+    for elemento in nivel:
+        linea = ''
+        for subelemento in elemento:
+            linea = linea +' '+ subelemento
+
+        print ("1 | " + (linea))
