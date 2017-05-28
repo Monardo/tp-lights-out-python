ebbcaf8737a84fd57cf7409067c95f1df7ccb52a
Mono <aledrienisienia@gmail.com>
2017-05-28 11:08:37 -0300

    refactor, mejoro el codigo.

--- modoPredeterminado.py
+++ modoPredeterminado.py
@@ -1,43 +1,25 @@
 # Este va a ser el modo predeterminado
 import nivel
 import tablero
+import pidoCoordenada
+import calculaSituacionJuego
 def iniciarModoPredeterminado():
-    nivelactual = 1
-    print("")
-    print("")
-    print("     *********************   BIENVENIDO AL MODO PREDETERMINADO   ********************")
-    print("")
-    print("")
-    nivelactual = nivel.getNivel(1)
-    tablero.mostrarNivel(nivelactual)
+    intentos = 0
+    while intentos < 15:
 
-    print("-------------------------------------------------")
+        print("")
+        print("")
+        print("     *********************   BIENVENIDO AL MODO PREDETERMINADO   ********************")
+        print("")
+        print("")
+        nivelInicio = nivel.getNivel(1)
+        tablero.mostrarNivel(nivelInicio)
 
-    listaLetraPermitidas = ['a', 'b', 'c', 'd', 'e']
-    listaNumerosPermitidos = ['1', '2', '3', '4', '5']
-    ingresoCoordenada = raw_input("Ingrese la coordenada :  ")
-    ingresoCoordenada.lower()
+        print("-------------------------------------------------")
 
-    if ingresoCoordenada == 'r':
-        print ("Ud ha seleccionado reiniciar el juego")
-        iniciarModoPredeterminado()
+        ingresoCoordenada = raw_input("Ingrese la coordenada :  ")
+        ingresoCoordenada.lower()
 
-    if len(ingresoCoordenada) == 2:
-        if ingresoCoordenada[0] not in listaLetraPermitidas or ingresoCoordenada[1] not in listaNumerosPermitidos:
-            print ("Ud ha ingresado una coordenada invalida. ")
-            iniciarModoPredeterminado()
-    else:
-        print ("Ud ha ingresado una coordenada invalida. ")
-        iniciarModoPredeterminado()
-
-    coordenasEnLetrasAPosiciones = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4}
-
-
-    coordenadaNumerica = int(ingresoCoordenada[1])
-
-    coordenadaLetraTransformada = coordenasEnLetrasAPosiciones[ingresoCoordenada[0]]
-
-    coordenadaLetraTransformada = coordenadaLetraTransformada
-    print coordenadaLetraTransformada
-    print coordenadaNumerica - 1
+        pidoCoordenada.recibeCoordenadaDeJuego(ingresoCoordenada)
+        intentos += 1
 
