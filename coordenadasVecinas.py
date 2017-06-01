5843a4ba1f975da2e5223929f85f8a00f6b00118
Maq301 <aledrienisienia@gmail.com>
2017-06-01 19:56:51 -0300

    Funcion que devuelve las coordenadas vecinas de la coordenada ingresada por el jugador

--- coordenadasVecinas.py
+++ coordenadasVecinas.py
@@ -1,4 +1,28 @@
 
-def devuelveCoordenadasVecinas(tablero, coordenadas):
+def eliminaVecinosInvalidos(vecinos):
+
+    vecinosValidos = []
+
+    for tupla in vecinos:
+
+        if tupla[0] >= 0 and tupla[1] >= 0:
+
+            vecinosValidos.append(tupla)
+
+    return vecinosValidos
 
 
+def devuelveCoordenadasVecinas(tablero, coordenadaEnTupla):
+    vecinos = []
+
+    fila = coordenadaEnTupla[0]
+    columna = coordenadaEnTupla[0]
+
+    coordenadaVecinoSuperior = (fila-1,columna)
+    coordenadaVecinoInferior = (fila+1, columna)
+    coordenadaVecinoIzquierda = (fila, columna-1)
+    coordenadaVecinoDerecha = (fila, columna+1)
+
+    vecinos = [coordenadaVecinoSuperior, coordenadaVecinoInferior, coordenadaVecinoIzquierda, coordenadaVecinoDerecha]
+
+    return eliminaVecinosInvalidos(vecinos)
