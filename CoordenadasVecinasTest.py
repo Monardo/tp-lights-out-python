5ef2b376920c68d06fc9a6a74a3a57a95f5ed3d2
Maq301 <aledrienisienia@gmail.com>
2017-06-01 19:56:11 -0300

    Funcion que testea si las coordenadas vecinas son validas

---
+++ CoordenadasVecinasTest.py
@@ -0,0 +1,29 @@
+import unittest
+import coordenadasVecinas
+
+
+class CoordenadasVecinasTestCase(unittest.TestCase):
+
+    def test_devuelveCoordenadasVecinasDevuelve4vecinosParaUnCentroDelTablero(self):
+
+        tablero = [["o",".","."],["o",".","."],["o",".","."]]
+        coordenadaCentral = (1,1)
+
+        vecinos = coordenadasVecinas.devuelveCoordenadasVecinas(tablero,coordenadaCentral)
+        self.assertEqual(len(vecinos),4)
+
+
+    def test_devuelveCoordenadasVecinasDevuelve3vecinosParaUnCentroSuperiorDelTablero(self):
+        tablero = [["o", ".", "."], ["o", ".", "."], ["o", ".", "."]]
+        coordenadaCentral = (0, 1)
+        vecinos = coordenadasVecinas.devuelveCoordenadasVecinas(tablero, coordenadaCentral)
+        self.assertEqual(len(vecinos), 3)
+
+
+    def test_devuelveCoordenadasVecinasDevuelve2vecinosParaUnVerticeDelTablero(self):
+        tablero = [["o", ".", "."], ["o", ".", "."], ["o", ".", "."]]
+        coordenadaCentral = (0, 0)
+        vecinos = coordenadasVecinas.devuelveCoordenadasVecinas(tablero, coordenadaCentral)
+        self.assertEqual(len(vecinos), 2)
+
+
