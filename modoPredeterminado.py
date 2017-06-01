79fb32119f07eb12ae7ea08eb585b533f021b108
Maq301 <aledrienisienia@gmail.com>
2017-06-01 19:59:07 -0300

    refactor

--- modoPredeterminado.py
+++ modoPredeterminado.py
@@ -3,6 +3,7 @@ import nivel
 import tablero
 import pidoCoordenada
 import calculaSituacionJuego
+
 def iniciarModoPredeterminado():
     intentos = 0
     while intentos < 15:
@@ -21,5 +22,6 @@ def iniciarModoPredeterminado():
         ingresoCoordenada.lower()
 
         pidoCoordenada.recibeCoordenadaDeJuego(ingresoCoordenada)
+
         intentos += 1
 
