f9f4594452140ed1c54733ded784e17fd6dffe21
Maq301 <aledrienisienia@gmail.com>
2017-06-06 19:23:07 -0300

    arreglo coordenadaColumna[1]

--- modificoTablero.py
+++ modificoTablero.py
@@ -1,8 +1,22 @@
-import nivel
-import modoPredeterminado
-def coordenadaCambiaMatriz(nivelactual, coordenadas):
+import coordenadasVecinas
 
-    tablero = nivel.getNivel(nivelactual)
-    coordenadasVecinas = getCoordenadasVecinas(tablero,coordenadas)
+def coordenadaCambiaMatriz(tablero, coordenadaEnTupla):
+
+    vecinas = coordenadasVecinas.devuelveCoordenadasVecinas(tablero,coordenadaEnTupla)
+
+    if tablero[coordenadaEnTupla[0]] [coordenadaEnTupla[1]] == "o":
+        tablero[coordenadaEnTupla[0]] [coordenadaEnTupla[1]] = "."
+    else:
+        tablero[coordenadaEnTupla[0]] [coordenadaEnTupla[1]] = "o"
 
     #recorrer la lista de coordernadas vecinas y por cada una cambiar su valor, tambien cambiar el valor de la coordenadas
+
+    for elemento in vecinas:
+        fila = int(elemento[0])
+        columna = int(elemento[1])
+        if tablero [fila][columna] == "o":
+            tablero [fila][columna] = "."
+        else:
+            tablero[fila][columna] = "o"
+
+    return tablero
